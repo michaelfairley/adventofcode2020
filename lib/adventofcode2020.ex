fa53e12e2300e9b7ab07eb3f36d4ba8093572c74
Michael Fairley <michaelfairley@gmail.com>
2020-11-30 23:06:21 -0800

    1a

---
+++ lib/adventofcode2020.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcode2020 do
+  @moduledoc """
+  Documentation for `Adventofcode2020`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode2020.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
